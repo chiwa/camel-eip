69104a0d83a125e1cd3fdd1102631f7640698e40
chiwakantawong <chiwa.k@tcrbank.com>
2025-08-28 18:37:41 +0700

    Initial commit - EP0 + EP1 basic routes

---
+++ src/main/java/com/zengcode/eip/camel/CamelEipApplication.java
@@ -0,0 +1,13 @@
+package com.zengcode.eip.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelEipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelEipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zengcode/eip/camel/CamelEipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zengcode.eip.camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelEipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
